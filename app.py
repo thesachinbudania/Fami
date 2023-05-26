a9f3657b79bdf13b3a7296f366e41941b117ff05
Sachin Budania <thesachinbudania@gmail.com>
2023-05-26 12:21:45 +0530

    designed the sign up page

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, request, render_template, redirect
+from werkzeug.security import check_password_hash, generate_password_hash
+from cs50 import SQL
 
 app = Flask(__name__)
+
+db = SQL("sqlite:///fami.db")
 
 @app.route("/")
 def main():
@@ -12,3 +16,12 @@ def main():
 def signup():
     if request.method == "GET":
         return render_template("signup.html")
+    else:
+        familyNameUq = request.form.get("familyNameUq")
+        familyMembers = request.form.get("familyMembers")
+        familyNN = request.form.get("familyNN")
+        password = request.form.get("password")
+        if not db.execute("SELECT * FROM account_info WHERE name = ?", familyNameUq)[0]:
+            return render_template("signup.html", error="Family Name taken choose another")
+        else:
+            hash = generate_password_hash(password)
